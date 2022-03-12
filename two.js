9b65f6da6bbb33425282d38e0b59f27a5aaa2e8b
projex <81651277+terezxmr@users.noreply.github.com>
2022-03-12 21:16:00 +0300

    Create two.js

---
+++ two.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <p id="hello hithub & NOSANA"></p>
+
+    <script>
+      document.getElementById("hello").innerHTML = "Hello, world!";
+    </script>
+  </body>
+</html>
